7a57c7ca24caed7b30f9240fb901319f110684d8
melvinr <melvinr@128.189.028.145.hva.nl>
2016-04-21 13:41:49 +0200

    Added files again
    There was a git pull error on the server

---
+++ routes/api.js
@@ -0,0 +1,55 @@
+var fs = require('fs'),
+    express = require('express'),
+    router = express.Router(),
+    jsonfile = require('jsonfile'),
+    moment = require('momentjs'),
+    getLastObject = require('../methods/methods.js');
+
+router.post('/', function(req, res) {
+    var file = 'resources/data.json',
+        now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    jsonfile.readFile(file, function(err, obj) {
+        var lastObject = getLastObject(obj),
+            newdata = {
+                time: now,
+                input: {
+                    motion: req.body.input || lastObject.input.motion
+                },
+                output: {
+                    led: req.body.output || lastObject.output.led
+                }
+            };
+        console.log(req.body)
+        obj.push(newdata);
+        jsonfile.writeFileSync(file, obj);
+        res.redirect('/');
+    });
+});
+
+router.get('/status/input', function(req, res) {
+    var file = 'resources/data.json';
+
+    jsonfile.readFile(file, function(err, obj) {
+        res.send('{"motion":"' + getLastObject(obj).input.motion + '"}');
+    });
+});
+
+router.get('/status/output', function(req, res) {
+    var file = 'resources/data.json';
+
+    jsonfile.readFile(file, function(err, obj) {
+        res.send('{"led":"' + getLastObject(obj).output.led + '"}');
+    });
+});
+
+router.get('/data/', function(req, res) {
+    var name = req.params.name;
+    var file = 'resources/data.json';
+
+    jsonfile.readFile(file, function(err, obj) {
+        res.send(obj);
+    });
+});
+
+module.exports = router;
